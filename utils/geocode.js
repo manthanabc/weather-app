f54c6e979db0c36edef5f99bae0ccecac4473618
manthanmabc <manthanmji@gamil.com>
2021-01-21 17:36:56 +0530

    fixed a bug that was causing the app to crash when no lat long was found

--- utils/geocode.js
+++ utils/geocode.js
@@ -6,7 +6,7 @@ const geocode=(address,callback)=>{
     request({ url, json:true },(err, {body})=>{
         if(err){
            callback('unable to access mapbox.',undefined) 
-        } else if (body.features.length === 0) {
+        } else if (!body.features) {
            callback('unable to find location.',undefined)  
         } else {
            callback(undefined, { latitude:body.features[0].center[1], longitude:body.features[0].center[0], place:body.features[0].place_name})
